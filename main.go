8dcc201f28391db9142cc0605a461e7b18092d27
Liedsonfsa <fabricoliedson@gmail.com>
2025-02-19 15:46:29 -0300

    feat: creating POST and GET routes and testing requests

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"rest-api/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	http.HandleFunc("GET /events", nil)
-	http.HandleFunc("POST /events", nil)
-	http.HandleFunc("GET /events/{id}", nil)
+	server := gin.Default()
 
-	http.ListenAndServe(":5000", nil)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message":"Event created!", "event": event})
 }
